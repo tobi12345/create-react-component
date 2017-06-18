a45520e12c8474aa6571f4c780a5c5bc96f5cd11
tobi12345 <t.klesel@tum.de>
2017-06-18 21:57:16 +0200

    css qustion added

--- extension.js
+++ extension.js
@@ -1,10 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
+const util = require('util')
+
 var fs = require('fs');
 var path = require('path');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+
+
+
 function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -19,7 +25,11 @@ function activate(context) {
             if (typeof name === "undefined") {
                 return vscode.window.showErrorMessage("Create Component Failed");
             }
-            createComponent(name, context.fsPath, true);
+
+            vscode.window.showQuickPick(["yes", "no"], { placeHolder: "Do you need a css file?" }).then((needCss) => {
+                createComponent(name, needCss !== "no", context.fsPath, true);
+
+            });
         });
     });
     var disposable = vscode.commands.registerCommand('extension.addComponentStateless', function (context) {
@@ -27,7 +37,10 @@ function activate(context) {
             if (typeof name === "undefined") {
                 return vscode.window.showErrorMessage("Create Component Failed");
             }
-            createComponent(name, context.fsPath, false);
+            vscode.window.showQuickPick(["yes", "no"], { placeHolder: "Do you need a css file?" }).then((needCss) => {
+                createComponent(name, needCss !== "no", context.fsPath, false);
+
+            });
         });
     });
 
@@ -66,10 +79,10 @@ function writeFile(file, data) {
 }
 
 
-function componnentString(name) {
+function componnentString(name, needCss) {
     return `import React from 'react';
 
-import './${name}.css';
+${needCss ? `import './${name}.css';` : ""}
 
 class ${name} extends React.Component {
     constructor(props) {
@@ -78,7 +91,7 @@ class ${name} extends React.Component {
 
     render() {
         return (
-            <div className='${name.toLowerCase()}_wrapper'>
+            <div ${needCss ? `className='${name.toLowerCase()}_wrapper'` : ""}>
 
             </div>
         );
@@ -89,15 +102,15 @@ export default ${name};`
 
 }
 
-function componnentString_Stateless(name) {
+function componnentString_Stateless(name, needCss) {
     return `import React from 'react';
 
-import './${name}.css';
+${needCss ? `import './${name}.css';` : ""}
 
 const ${name} = () => {
 
     return (
-        <div className='${name.toLowerCase()}_wrapper'>
+        <div ${needCss ? `className='${name.toLowerCase()}_wrapper'` : ""}>
 
         </div>
     );
@@ -112,7 +125,7 @@ function cssString(name) {
     return `.${name.toLowerCase()}_wrapper{\n\n}`
 }
 
-function createComponent(name, _path, isStateful) {
+function createComponent(name, needCss, _path, isStateful) {
     const dir_path = _path;
     const component_name = name;
     const file_name_react = path.join(dir_path, component_name, component_name + ".react.js");
@@ -123,7 +136,7 @@ function createComponent(name, _path, isStateful) {
 
     const file_Promise_Component = createFile(file_name_react);
     file_Promise_Component.then((file) => {
-        const write_Promise_Component = writeFile(file, isStateful ? componnentString(component_name) : componnentString_Stateless(component_name));
+        const write_Promise_Component = writeFile(file, isStateful ? componnentString(component_name, needCss) : componnentString_Stateless(component_name, needCss));
 
         write_Promise_Component.then(() => {
             fs.close(file, function (err) {
@@ -140,6 +153,7 @@ function createComponent(name, _path, isStateful) {
         console.log(err);
         vscode.window.showInformationMessage('Error');
     });
+    if (!needCss) return;
 
     const file_Promise_CSS = createFile(file_name_css);
     file_Promise_CSS.then((file) => {
